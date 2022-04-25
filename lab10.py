bc61858321eccd1120c7708a5818939cb1f8cedc
Valmiki Kothare <valmiki.kothare.vk@gmail.com>
2022-04-25 18:22:38 -0400

    Changed turn values

--- lab10.py
+++ lab10.py
@@ -60,13 +60,13 @@ def follow_line_until_black(er, state):
         light_diff = light - LIGHT_THRESHOLD
         if light_diff < 0:
             # white
-            dps_add = np.array((1.0, -1.0))
+            dps_add = np.array((1.0, -0.5))
             p_term = abs(light_diff) * LINE_P_MULTIPLIER * dps_add
             p_term = np.minimum(p_term, np.array((4, 0)))
             time_in_black = 0
         else:
             # black
-            dps_add = np.array((-1.0, 1.0))
+            dps_add = np.array((-0.5, 1.0))
             p_term = abs(light_diff) * LINE_P_MULTIPLIER * dps_add
             time_in_black += loop_duration
             if time_in_black > BLACK_TIME_THRESHOLD:
@@ -78,6 +78,42 @@ def follow_line_until_black(er, state):
         time.sleep(max(0, LOOP_DURATION - duration))
 
         loop_duration = time.perf_counter() - start_time
+
+def follow_line_until_waypoint(er, state, waypoint):
+    while True:
+        start_time = time.perf_counter()
+
+        # Update state
+        encoders_diff = er.read_encoders_diff()
+        state = update_state(state, encoders_diff)
+        
+        pos = state_to_pos(state)
+        if np.linalg.norm(pos - waypoint) < PROXIMITY_THRESHOLD:
+            return
+
+        # Read from sensors
+        light = BP.get_sensor(LIGHT_SENSOR_PORT)
+
+        # Follow line
+        dps = STRAIGHT_MOTOR_DPS
+        dps_add = None
+        p_term = None
+
+        light_diff = light - LIGHT_THRESHOLD
+        if light_diff < 0:
+            # white
+            dps_add = np.array((1.0, -1.0))
+            p_term = abs(light_diff) * LINE_P_MULTIPLIER * dps_add
+            p_term = np.minimum(p_term, np.array((4, 0)))
+        else:
+            # black
+            dps_add = np.array((-1.0, 1.0))
+            p_term = abs(light_diff) * LINE_P_MULTIPLIER * dps_add
+        dps = dps + p_term
+        move.set_motor_dps(dps)
+
+        duration = time.perf_counter() - start_time
+        time.sleep(max(0, LOOP_DURATION - duration))
 
 def move_straight_until_white(er, move, state):
     # Move past black border until reach white
@@ -135,41 +171,7 @@ def move_straight_until_black(er, move, state):
 
         loop_duration = time.perf_counter() - start_time
 
-def follow_line_until_waypoint(er, state, waypoint):
-    while True:
-        start_time = time.perf_counter()
 
-        # Update state
-        encoders_diff = er.read_encoders_diff()
-        state = update_state(state, encoders_diff)
-        
-        pos = state_to_pos(state)
-        if np.linalg.norm(pos - waypoint) < PROXIMITY_THRESHOLD:
-            return
-
-        # Read from sensors
-        light = BP.get_sensor(LIGHT_SENSOR_PORT)
-
-        # Follow line
-        dps = STRAIGHT_MOTOR_DPS
-        dps_add = None
-        p_term = None
-
-        light_diff = light - LIGHT_THRESHOLD
-        if light_diff < 0:
-            # white
-            dps_add = np.array((1.0, -1.0))
-            p_term = abs(light_diff) * LINE_P_MULTIPLIER * dps_add
-            p_term = np.minimum(p_term, np.array((4, 0)))
-        else:
-            # black
-            dps_add = np.array((-1.0, 1.0))
-            p_term = abs(light_diff) * LINE_P_MULTIPLIER * dps_add
-        dps = dps + p_term
-        move.set_motor_dps(dps)
-
-        duration = time.perf_counter() - start_time
-        time.sleep(max(0, LOOP_DURATION - duration))
 
 if __name__ == "__main__":
     try:
@@ -206,8 +208,14 @@ if __name__ == "__main__":
 
             move_straight_until_white(er, move, state)
 
-
+            follow_line_until_waypoint(er, state, np.array((0, 0)))
             
+            # Orient to original pose
+            move.turn(85, state)
+
+            light = BP.get_sensor(LIGHT_SENSOR_PORT)
+            light_diff = light - LIGHT_THRESHOLD
+            black = light_diff >= 0
 
     except KeyboardInterrupt:
         print("Interrupted")
